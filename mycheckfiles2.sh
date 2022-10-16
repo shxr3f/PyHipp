2aed2b033912aafe147f53fbe65870eea04b515a
AWS ParallelCluster user <ec2-user@ip-10-0-15-188.ap-southeast-1.compute.internal>
2022-10-16 07:08:38 +0000

    Adding my slurm files:

---
+++ mycheckfiles2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Number of hkl files"
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+
+echo "Number of mda files"
+find mountains -name "firings.mda" | wc -l
+
+echo "Start Times"
+head -1 *.out
+
+echo "End Times"
+tail --lines=5 *.out
